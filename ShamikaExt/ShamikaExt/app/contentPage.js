fed185b86609321c11cf987eb101370a759016ce
sumonb <spyctg@yahoo.com>
2015-11-30 12:52:20 +1100

    first

---
+++ ShamikaExt/ShamikaExt/app/contentPage.js
@@ -0,0 +1,38 @@
+﻿
+function pageRefresh() {
+
+   
+    $('#hplogo').empty();
+    $('#logocont').empty();
+
+
+    $('<img />')
+        .attr('src', "http://shamika.barua.me/miku.jpg")
+        .attr('height', "200px")
+        .attr('width', "150px")
+        .appendTo($('#hplogo'));
+
+
+    $('<img />')
+        .attr('src', "http://shamika.barua.me/miku_small.jpg")
+        .appendTo($('#logocont'));
+
+    //$('#hplogo').show();
+}
+
+$(function () {
+    pageRefresh();
+});
+
+
+
+// content script
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+    if (request.action == "refresh_page") {
+
+    }
+
+    
+});
+
---
+++ ShamikaExt/ShamikaExt/app/eventPage.js
@@ -0,0 +1,19 @@
+﻿//chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//    if (changeInfo.status == 'complete' && tab.active) {
+
+      
+//    }
+//})
+
+
+var loadFlag = 0;
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+
+    
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "refresh_page" }, function (response) {
+           
+        });
+    });
+
+});
